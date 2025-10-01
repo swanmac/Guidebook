ea66e75696c7f1f9952f07ae2185d62658613174
Greg Swan <gmswan99@gmail.com>
2025-10-01 13:10:07 -0400

    create city

---
+++ Guidebook/City.swift
@@ -0,0 +1,15 @@
+//
+//  City.swift
+//  Guidebook
+//
+//  Created by Greg Swan on 10/1/25.
+//
+
+import Foundation
+
+struct City {
+    
+    var name: String
+    var summary: String
+    var imageName: String
+}
---
+++ Guidebook/DataService.swift
@@ -0,0 +1,12 @@
+//
+//  DataService.swift
+//  Guidebook
+//
+//  Created by Greg Swan on 10/1/25.
+//
+
+import Foundation
+
+struct DataService {
+    
+}
