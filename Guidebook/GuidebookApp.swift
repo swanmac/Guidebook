5cc771512a7750187d7819390f51f1894ce0da51
Greg Swan <gmswan99@gmail.com>
2025-10-01 12:51:18 -0400

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Greg Swan on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
