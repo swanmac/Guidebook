bfdf329e06db7e4dc15fa5ac4b3ece941b95c6ae
Greg Swan <gmswan99@gmail.com>
2025-10-01 13:15:16 -0400

    create attraction

---
+++ Guidebook/Attraction.swift
@@ -0,0 +1,17 @@
+//
+//  Attraction.swift
+//  Guidebook
+//
+//  Created by Greg Swan on 10/1/25.
+//
+
+import Foundation
+
+struct Attraction {
+    
+    var name: String
+    var summary: String
+    var longDescription: String
+    var imageName: String
+    var latLong: String
+}
