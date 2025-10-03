2f3843c61dd4fb9758f3cdf0885d04ab7f233410
Greg Swan <gmswan99@gmail.com>
2025-10-03 19:26:37 -0400

    create attraction row

---
+++ Guidebook/AttractionRow.swift
@@ -0,0 +1,20 @@
+//
+//  AttractionRow.swift
+//  Guidebook
+//
+//  Created by Greg Swan on 10/3/25.
+//
+
+import SwiftUI
+
+struct AttractionRow: View {
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+
+
+#Preview {
+    CityView()
+}
