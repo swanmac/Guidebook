88670e1177840ddf08fe3222fdc9758928e2374d
Greg Swan <gmswan99@gmail.com>
2025-10-06 19:15:45 -0400

    change cityview from getData to getFileDate

--- Guidebook/CityView.swift
+++ Guidebook/CityView.swift
@@ -37,7 +37,7 @@ struct CityView: View {
         }
         .ignoresSafeArea()
         .onAppear {
-            cities = dataService.getData()
+            cities = dataService.getFileData()
         }
     }
 }
